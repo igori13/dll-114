6c7217de172a3f4744d1b6421b5faed7edd8a184
306 <306@306NEW-7>
2023-07-05 11:22:32 +0300

    Добавьте файлы проекта.

---
+++ dll 114/Hobby.cs
@@ -0,0 +1,27 @@
+﻿// Студет Корнилов Артем
+using System;
+
+namespace MyLibrary
+{
+    public class Hobby : IComparable<Hobby>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Hobby(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Hobby: Name={Name}, Description={Description}");// Студет Корнилов Артем
+        }
+
+        public int CompareTo(Hobby other)
+        {
+            return string.Compare(Name, other.Name, StringComparison.Ordinal);// Студет Корнилов Артем
+        }
+    }
+}
---
+++ dll 114/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLibrary// Студет Корнилов Артем
+{
+    public class Person// Студет Корнилов Артем
+    {
+        public string Address { get; set; }
+        public List<Hobby> Hobbies { get; set; }
+
+        public Person(string address)
+        {
+            Address = address;
+            Hobbies = new List<Hobby>();
+        }
+
+        public void AddHobby(Hobby hobby)// Студет Корнилов Артем
+        {
+            Hobbies.Add(hobby);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Person: Address={Address}");// Студет Корнилов Артем
+        }
+
+        public void ShowAll()
+        {
+            foreach (Hobby hobby in Hobbies)// Студет Корнилов Артем
+            {
+                hobby.Show();
+            }
+        }
+    }
+}
